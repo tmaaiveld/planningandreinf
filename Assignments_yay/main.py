22ad5ae54456c2dbd78baa1f81d72ed4596de92b
tmaaiveld <35788671+tmaaiveld@users.noreply.github.com>
2019-05-03 11:38:27 +0200

    correctly implemented the running average of runtime (I think, needs to be checked!). Is a NUMBER_OF_ALGORITHMS x NUMBER_OF_GAMMAS matrix, rows are algorithm indices, columns are gamma values.

--- Assignments_yay/main.py
+++ Assignments_yay/main.py
@@ -6,8 +6,6 @@ from Policy_Evaluation import policy_evaluation
 from Howards_Policy_Iteration import howards_policy_iteration
 from Simple_Policy_Iteration import simple_policy_iteration
 from Value_Iteration import value_iteration
-# from Assignments_yay.HiddenPrints import HiddenPrints
-from HiddenPrints import HiddenPrints
 
 #  Constants
 ALGORITHMS = ["Random Policy Evaluation", "Value Iteration", "Howard's Policy Iteration", "Simple Policy Iteration"]
@@ -15,6 +13,7 @@ GAMMA_RANGE = [0.9, 1]
 GAMMA_INCREMENT = 0.01
 THETA = 0.0001 
 NUMBER_OF_RUNS = 3
+
 
 def print_header(title):
     print("\n" + "-" * (len(title) + 6))
@@ -51,6 +50,7 @@ def print_policy(policy):
         pols = np.array(pols)
         print(np.array2string(np.flip(pols,axis=0), separator=',', formatter={'str_kind': lambda x: x}))
 
+
 def print_moves(policy):
     solution_matrix = []
     for row in policy:
@@ -77,6 +77,7 @@ def append_results(algorithm, V, policy, cycles, convergence_time):
     cycle_counts[ALGORITHMS.index(algorithm)].append(cycles)
     convergence_times[ALGORITHMS.index(algorithm)].append(convergence_time)
 
+
 def plot_iterations_gamma():
     cycles_gamma = plt.figure() 
     for algorithm_index in range(4):    
@@ -88,6 +89,7 @@ def plot_iterations_gamma():
     plt.ylabel("iterations")
     plt.savefig('Iterations_gamma.eps')
     plt.show()
+
 
 def plot_runtime_gamma():
     runtime_gamma = plt.figure()
@@ -117,17 +119,14 @@ policy_tabs = [[], [], [], []]
 cycle_counts = [[], [], [], []]  # 3-D arrays. Dimensions: value, iteration, algorithm index.
 convergence_times = [[], [], [], []]
 
-### create lists that store sums of runtimes (and then averages are taken at the end)
-sum_rand = [[], [], [], [], [], [], [], [], [], []]
-sum_val = [[], [], [], [], [], [], [], [], [], []]
-sum_how = [[], [], [], [], [], [], [], [], [], []]
-sum_sim = [[], [], [], [], [], [], [], [], [], []] 
+#  Create lists that stores averages of runtimes
+running_averages = np.zeros([len(ALGORITHMS),int(round((GAMMA_RANGE[1]-GAMMA_RANGE[0])/GAMMA_INCREMENT))])
 
 for run in range(NUMBER_OF_RUNS):
 
     print('\n _ __ _  This is the ' + str(run) + 'th run   _ __ _  ')
  
-    # I did implement parameter tuning, but it seems useless. Algos break for all values over 0.91 (Down on cell [2,3]).
+    i = 0
     for gamma in np.flip(gammas):
         gamma = round(gamma, len(str(GAMMA_INCREMENT))-2)
         print("\n\n*** Running for gamma = {} ***".format(gamma))
@@ -156,50 +155,25 @@ for run in range(NUMBER_OF_RUNS):
         print_results(V, policy, cycles, time, gamma)
         append_results(ALGORITHMS[3], V, policy, cycles, time)
 
+        # store runtimes for each gamma
+        for algorithm in range(len(ALGORITHMS)):
+            running_averages[algorithm][i] = convergence_times[algorithm][i] * (1 / NUMBER_OF_RUNS)
+        i += 1
 
     #  Results
     V_tabs = np.array(V_tabs)
 
-    ###store runtimes in empty nested-lists above the big loop
-    for gamma_index in range(10):
-        sum_rand[gamma_index].append(convergence_times[0][gamma_index])
-    
-    for gamma_index in range(10):
-        sum_val[gamma_index].append(convergence_times[1][gamma_index])
-        
-    for gamma_index in range(10):
-        sum_how[gamma_index].append(convergence_times[2][gamma_index])
-
-    for gamma_index in range(10):
-        sum_sim[gamma_index].append(convergence_times[3][gamma_index])
-
-    ###empty the results of this run to start the next one fresh
+    #  Empty the results of this run to start the next one fresh
     V_tabs = [[], [], [], []]  # 4-D arrays. Dimensions: table.x, table.y, iteration, algorithm index.
     policy_tabs = [[], [], [], []]
 
     cycle_counts = [[], [], [], []]  # 3-D arrays. Dimensions: value, iteration, algorithm index.
     convergence_times = [[], [], [], []]
 
-###Take averages to get average run times for each algorithm for each gamma
-avg_rand = []
-for lst in sum_rand:
-    avg_rand.append(sum(lst)/len(lst))
- 
-avg_val = []
-for lst in sum_val:
-    avg_val.append(sum(lst)/len(lst))
+print(running_averages)
+exit()
 
-avg_how = []
-for lst in sum_how:
-    avg_how.append(sum(lst)/len(lst))
-
-avg_sim = []
-for lst in sum_sim:
-    avg_sim.append(sum(lst)/len(lst))
-
-averages_runtimes = [avg_rand, avg_val, avg_how, avg_sim]
-
-###Print run times for all algos and all gammas (averages)
+#  Print run times for all algos and all gammas (averages)
 runtime_gamma_average = plt.figure()
 for algorithm_index in range(4): 
     plt.plot(np.flip(gammas), averages_runtimes[algorithm_index], label= str(ALGORITHMS[algorithm_index])) 
